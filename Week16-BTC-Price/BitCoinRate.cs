67a63f725d379edbc0498fa81662b9a13d49aaf8
KristjanKaa <159649951+KristjanKaa@users.noreply.github.com>
2024-02-24 20:55:42 +0200

    Added my project

---
+++ Week16-BTC-Price/BitCoinRate.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Week16_BTC_Price
+{
+    public class BitCoinRate
+    {
+        public Bpi bpi { get; set; }
+    }
+
+    public class Bpi
+    {
+        public USD USD { get; set; }
+    }
+
+    public class USD
+    {
+        public string code { get; set; }
+        public float rate_float { get; set; }
+    }
+}
+   
+   
---
+++ Week16-BTC-Price/Program.cs
@@ -0,0 +1,27 @@
+﻿
+
+using Newtonsoft.Json;
+using System.Net;
+using Week16_BTC_Price;
+
+BitCoinRate bitcoin = GetRates();
+
+Console.WriteLine($"Praegune BTC hind $:  {bitcoin.bpi.USD.rate_float} {bitcoin.bpi.USD.code}");
+
+static BitCoinRate GetRates()
+{
+    string url = "https://api.coindesk.com/v1/bpi/currentprice.json";
+    HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
+    request.Method = "GET";
+
+    var webResponse = request.GetResponse();
+    var webStreame = webResponse.GetResponseStream();
+
+    BitCoinRate bitcoinRate;
+    using (var responseReader = new StreamReader(webStreame))
+    {
+        var response = responseReader.ReadToEnd();
+        bitcoinRate = JsonConvert.DeserializeObject<BitCoinRate>(response);
+    }
+    return bitcoinRate;
+}
